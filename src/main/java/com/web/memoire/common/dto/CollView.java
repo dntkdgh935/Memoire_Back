c595d772cdf37c4a97c00c41a8a609a4032877ae
seonbint <seonbint@gmail.com>
2025-07-09 16:24:43 +0900

    React에 전체 컬렉션 불러오기(img)
    - 일단 이미지 썸네일만 가져옴 (영상/ 텍스트 처리필요)
    - 임시 유저(user001)사용

---
+++ src/main/java/com/web/memoire/common/dto/CollView.java
@@ -0,0 +1,44 @@
+package com.web.memoire.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CollView {
+
+    private String collectionid;
+
+    private String authorid;
+
+    private String collectionTitle;
+
+    private int readCount;
+
+    private int visibility;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date createdDate;
+
+    private String titleEmbedding;
+
+    private String viewType;
+
+    private String thumbnailPath;
+
+    private String textContent;
+
+    private boolean userlike;
+
+    private boolean userbookmark;
+
+    private String authorProfileImage;
+
+    private String thumbType;
+
+}
---
+++ src/main/java/com/web/memoire/library/jpa/repository/LibCollectionRepository.java
@@ -0,0 +1,13 @@
+package com.web.memoire.library.jpa.repository;
+
+import com.web.memoire.common.entity.CollectionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface LibCollectionRepository extends JpaRepository<CollectionEntity, String> {
+
+    List<CollectionEntity> findByVisibility(int visibility);
+
+}
