396d24387ee54425bc9cce16258b642490a51a8b
dntkdgh935 <dntkdgh935@naver.com>
2025-07-14 16:12:10 +0900

    변경

--- src/main/java/com/web/memoire/atelier/video/config/RestTemplateConfig.java
+++ src/main/java/com/web/memoire/atelier/video/config/RestTemplateConfig.java
@@ -1,13 +1,13 @@
-package com.web.memoire.atelier.video.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class RestTemplateConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
+//package com.web.memoire.atelier.video.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Configuration
+//public class RestTemplateConfig {
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//}
